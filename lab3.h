caa6816f30de1169a756c469c5726f048e5210cc
changyuanhua <chang19960726@gmail.com>
2016-04-05 01:25:27 +0800

    還沒makefile

---
+++ lab3.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+class lab3
+{
+	public:
+		int main();
+		void readfile();
+		void check();
+		void sort();
+	private:		
+		int A[10000],a;
+};
